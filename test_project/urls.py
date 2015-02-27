a9a83bf9d4bd9807dd70b5839071aa4d4422f8ca
winzard <max.vasenkov@gmail.com>
2015-02-27 11:41:47 +0300

    Temporary fix DoesNotExist. Still has no ideas about thread safe. fixed urls for Django 1.6

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls.defaults import patterns, include, url
+
 
 # Uncomment the next two lines to enable the admin:
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
